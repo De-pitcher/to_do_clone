e8d65468af433c10eb199bc0a01914a8e345b09c
Olaoluwa <tokiolaoluwa01@gmail.com>
2023-01-16 15:32:56 +0100

    finished landing and profile screens

---
+++ lib/screens/profile.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class ProfileAccount extends StatefulWidget {
+  static const String id = '/profile_account_settings';
+  const ProfileAccount({super.key});
+
+  @override
+  State<ProfileAccount> createState() => _ProfileAccountState();
+}
+
+class _ProfileAccountState extends State<ProfileAccount> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          UserAccountsDrawerHeader(
+            currentAccountPicture: const CircleAvatar(),
+            accountName: const Text('User name'),
+            accountEmail: const Text('User Email Address'),
+            decoration: const BoxDecoration(),
+            onDetailsPressed: () => Navigator.of(context).pop(),
+          ),
+          const SizedBox(height: 10),
+          const Divider(
+            thickness: 2,
+            color: Colors.white12,
+            endIndent: 20,
+            indent: 20,
+          ),
+          actionTile(context, Icons.add, 'Add account'),
+          actionTile(context, Icons.manage_accounts, 'Manage accounts'),
+          const SizedBox(height: 10),
+          const Divider(
+            thickness: 2,
+            color: Colors.white12,
+            endIndent: 20,
+            indent: 20,
+          ),
+          const SizedBox(height: 10),
+          actionTile(context, Icons.settings, 'Settings'),
+        ],
+      ),
+    );
+  }
+
+  Widget actionTile(context, IconData icon, String action) {
+    return ListTile(
+      style: ListTileStyle.list,
+      onTap: () {},
+      leading: Icon(icon, color: Colors.white),
+      title: Text(
+        action,
+        style: const TextStyle(color: Colors.white),
+      ),
+    );
+  }
+}
